7d612dd92ceaaf961d1b6c2c41e3c29d767309e3
João Pimentel <32567686+citoplasme@users.noreply.github.com>
2023-12-01 15:57:37 +0000

    Added ,

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://web.cs.dal.ca/'
+  //site: 'https://web.cs.dal.ca/',
   //base: '/~pimentel',
   site: 'https://citoplasme.github.io'
 });
