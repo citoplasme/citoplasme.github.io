8ca01c1648917924b1d85bd9560aaf69e26d3f59
João Pimentel <32567686+citoplasme@users.noreply.github.com>
2024-05-03 16:31:03 +0100

    Added BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://web.cs.dal.ca/',
-  //base: '/~pimentel',
-  site: 'https://citoplasme.github.io'
+  //site: 'https://web.cs.dal.ca',
+  //base: '/~pimentel/',
+  site: 'https://citoplasme.github.io',
+  base: '/',
 });
